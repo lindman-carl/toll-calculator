e68698421bbd86baea7b95296c29ed601b997cea
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-04-25 18:04:55 +0200

    add: functionality to run script on external files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("hello");
+import { readFileSync } from "fs";
+
+import { calculateVehicleTollFee } from "./logic/tollCalculator";
+import { parseVehicle } from "./utils";
+
+const main = () => {
+  const fileName = process.argv[2];
+
+  // read file
+  const file = readFileSync(__dirname + "/" + fileName, "utf8");
+
+  // split file into lines
+  const lines = file.split("\n");
+
+  // parse lines
+  const vehicles = lines.map((line) => parseVehicle(line));
+
+  const tollFeeByVehicleId = new Map<string, number>();
+  let totalFee = 0;
+
+  console.log("Toll revenue report\n");
+
+  console.log("id".padEnd(24, " ") + "type".padEnd(24, " ") + "total fee");
+  // calculate toll fee for each vehicle, store in map and print
+  vehicles.forEach((vehicle) => {
+    const tollFee = calculateVehicleTollFee(vehicle);
+    tollFeeByVehicleId.set(vehicle.id, tollFee);
+    totalFee += tollFee;
+
+    console.log(
+      vehicle.id.padEnd(24, " ") + vehicle.type.padEnd(24, " ") + tollFee
+    );
+  });
+
+  console.group("\nStatistics");
+  console.log("total vehicles".padEnd(16, " ") + vehicles.length);
+  console.log("total fees".padEnd(16, " ") + totalFee);
+  console.log("average fee".padEnd(16, " ") + totalFee / vehicles.length);
+  console.groupEnd();
+};
+
+main();
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { z } from "zod";
+import { Vehicle } from "./types";
+
 export const getDateString = (date: Date) => {
   // format date to "month/day", "2/14"
   // 1-indexed
@@ -9,3 +12,38 @@ export const getMinutesSinceMidnight = (date: Date) =>
 
 export const getMinutesElapsed = (startTime: Date, endTime: Date) =>
   (endTime.getTime() - startTime.getTime()) / 1000 / 60;
+
+export const parseVehicle = (vehicle: string): Vehicle => {
+  const [id, type, tollPasses] = vehicle.split(";");
+
+  const tollPassesArray = tollPasses.split(",").map((tollPass) => {
+    return new Date(tollPass);
+  });
+
+  // input validation
+  const vehicleSchema = z.object({
+    id: z.string().nonempty(),
+    type: z.string().nonempty(),
+    tollPasses: z.array(z.date()),
+  });
+
+  const parsedVehicle: Vehicle = {
+    id,
+    type,
+    tollPassDates: tollPassesArray,
+  };
+
+  try {
+    vehicleSchema.parse({
+      id,
+      type,
+      tollPasses: tollPassesArray,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.error(error);
+    }
+  }
+
+  return parsedVehicle;
+};
