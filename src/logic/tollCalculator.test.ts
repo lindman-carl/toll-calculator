c155ff14fdb495916dcfc5615d4e9062169408d7
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-04-25 15:44:47 +0200

    add: isTollFreeDate

--- src/logic/tollCalculator.test.ts
+++ src/logic/tollCalculator.test.ts
@@ -1,6 +1,63 @@
-import { isTollFreeVehicleType } from "./tollCalculator";
-import { TOLL_FREE_VEHICLE_TYPES } from "../tollConfig.json";
+import { isTollFreeDate, isTollFreeVehicleType } from "./tollCalculator";
+import {
+  TOLL_FREE_VEHICLE_TYPES,
+  TOLL_FREE_DATES,
+  TOLL_FREE_WEEKDAYS,
+} from "../tollConfig.json";
 import { Vehicle } from "@/types";
+import { getDateString } from "../utils";
+
+describe("isTollFreedate", () => {
+  const tollFreeDatesMap = new Map<string, boolean>();
+  for (const date of TOLL_FREE_DATES) {
+    tollFreeDatesMap.set(date, true);
+  }
+
+  const tollFreeWeekdayDates: Date[] = [];
+  for (
+    let d = new Date(2021, 0, 1);
+    d <= new Date(2021, 11, 31);
+    d.setDate(d.getDate() + 1)
+  ) {
+    if (TOLL_FREE_WEEKDAYS.includes(d.getDay())) {
+      tollFreeWeekdayDates.push(d);
+      const dateString = getDateString(d);
+      tollFreeDatesMap.set(dateString, true);
+    }
+  }
+
+  test("should return true for all dates in TOLL_FREE_DATES", () => {
+    for (const date of TOLL_FREE_DATES) {
+      const [month, day] = date.split("/");
+      const testDate = new Date(2021, parseInt(month) - 1, parseInt(day), 0, 0);
+
+      const isTollFree = isTollFreeDate(testDate);
+      expect(isTollFree).toBe(true);
+    }
+  });
+
+  test("should return true for all weekend dates", () => {
+    for (const date of tollFreeWeekdayDates) {
+      const isTollFree = isTollFreeDate(date);
+      expect(isTollFree).toBe(true);
+    }
+  });
+
+  test("should return false for all other dates", () => {
+    for (
+      let d = new Date(2021, 0, 1);
+      d <= new Date(2021, 11, 31);
+      d.setDate(d.getDate() + 1)
+    ) {
+      const isTollFree = isTollFreeDate(d);
+      const dateString = getDateString(d);
+
+      if (!tollFreeDatesMap.has(dateString)) {
+        expect(isTollFree).toBe(false);
+      }
+    }
+  });
+});
 
 describe("isTollFreeVehicleType", () => {
   test("should return true for all vehicles with a type found in TOLL_FREE_VEHICLE_TYPES", () => {
--- src/logic/tollCalculator.ts
+++ src/logic/tollCalculator.ts
@@ -1,12 +1,55 @@
 import { z } from "zod";
 
 import { Vehicle } from "@/types";
-import { TOLL_FREE_VEHICLE_TYPES } from "../tollConfig.json";
+import { getDateString } from "../utils";
 
-// map toll free vehicle types for O(1) time complexity
+import {
+  TOLL_FREE_VEHICLE_TYPES,
+  TOLL_FREE_DATES,
+  TOLL_FREE_WEEKDAYS,
+} from "../tollConfig.json";
+
+// hash maps for O(1) time complexity
+const TOLL_FREE_DATES_MAP = new Map(TOLL_FREE_DATES.map((el) => [el, true]));
+const TOLL_FREE_WEEKDAYS_MAP = new Map(
+  TOLL_FREE_WEEKDAYS.map((el) => [el, true])
+);
 const TOLL_FREE_VEHICLE_TYPES_MAP = new Map(
   TOLL_FREE_VEHICLE_TYPES.map((el: string) => [el, true])
 );
+
+// functions
+export const isTollFreeDate = (date: Date): boolean => {
+  // returns true if date is toll free
+  // TOLL_FREE_DATES and TOLL_FREE_WEEKDAYS are defined in tollConfig.json
+  // O(1) time complexity
+
+  const month = date.getMonth();
+  const day = date.getDate();
+  const weekday = date.getDay();
+
+  // input validation
+  try {
+    z.number().min(0).max(11).parse(month);
+    z.number().min(1).max(31).parse(day);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.error(error);
+    }
+
+    // invalid input is not a toll free date
+    return false;
+  }
+
+  // check for toll free weekdays
+  if (TOLL_FREE_WEEKDAYS_MAP.has(weekday)) {
+    return true;
+  }
+
+  const currentDateString = getDateString(date);
+
+  return TOLL_FREE_DATES_MAP.has(currentDateString);
+};
 
 export const isTollFreeVehicleType = (vehicle: Vehicle): boolean => {
   // returns true if vehicle type is toll free
